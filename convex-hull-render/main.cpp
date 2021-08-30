aa964b99b987ef567a2bb343b9c16807213e6d01
Zeb Wu <wzb3422@gmail.com>
2021-08-30 20:35:55 +0800

    Initial Commit

---
+++ convex-hull-render/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  convex-hull-render
+//
+//  Created by Zhenbin Wu on 2021/8/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
